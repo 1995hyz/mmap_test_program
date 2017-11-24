27664f6163cc8da5515cb2e0084039d3c7fa8516
1995hyz <1995hyz@gmail.com>
2017-11-24 00:02:08 -0500

    Add files via upload

--- mtest1.c
+++ mtest1.c
@@ -5,12 +5,53 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+
+void handlerSegV();
+void handlerBus();
+void handlerKill();
+void handlerAbrt();
+void handlerIll();
+void runHandler();
 
 int fd=0;
 size_t length=4096;
 off_t offset=0;
 char *addr;
 char buf[2]={'a','\0'};
+
+void runHandler(){
+	signal(SIGSEGV,handlerSegV);
+	signal(SIGBUS,handlerBus);
+	signal(SIGKILL,handlerKill);
+	signal(SIGABRT,handlerAbrt);
+	signal(SIGILL,handlerIll);
+}
+
+void handlerSegV(){
+	fprintf(stderr,"Signal SIGSEGV Received\n");
+	exit(11);
+}
+
+void handlerBus(){
+	fprintf(stderr,"Signal SIGBUS Received\n");
+	exit(10);
+}
+
+void handlerKill(){
+	fprintf(stderr,"Signal SIGKILL Received\n");
+	exit(9);		//??????
+}
+
+void handlerAbrt(){
+	fprintf(stderr,"Signal SIGABRT Received\n");
+	exit(6);
+}
+
+void handlerIll(){
+	fprintf(stderr,"Signal SIGILL Received\n");
+	exit(4);
+}
 
 void main(int argc, char* argv[]){
 	if(!(fd=open("./mtext.txt",O_RDONLY))){
@@ -23,6 +64,7 @@ void main(int argc, char* argv[]){
 		fprintf(stderr,"Map Region Error: %s\n",strerror(errno));
 		exit(EXIT_FAILURE);
 	}
+	runHandler();
 	memcpy(addr,buf,strlen(buf)+1);
 	//write(STDOUT_FILENO,addr,1);
 	//printf("\n");
