4c23c4065689d8b9a77d9b7e18eb52c0d45fc6df
1995hyz <1995hyz@gmail.com>
2017-11-25 11:46:50 -0500

    A Basic Completed Version
    This is a basic completed version of the program. Some update will be uploaded later such as commands to remove test files.

--- mtest.c
+++ mtest.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 void handlerSegV();
 void handlerBus();
@@ -14,11 +15,25 @@ void handlerSys();
 void handlerAbrt();
 void handlerIll();
 void runHandler();
+//void fileMaker(int fileDes, int fileSize,char fileBuf[]);
 
 int fd=0;
 size_t length=4096;
 off_t offset=0;
 char *addr;
+
+/*void fileMaker(int fileDes,int fileSize,char fileBuf[]){
+	char bufBasic[10]={'0','1','2','3','4','5','6','7','8','9'};
+	 if(!(fileDes=open("./mtext.txt",O_RDWR|O_CREAT|O_TRUNC,0666))){
+                fprintf(stderr,"Open/Create mtext.txt Error: %s\n",strerror(errno));
+                exit(EXIT_FAILURE);
+        }
+        int i=0;
+        for(i;i<fileSize;i++){
+                strncat(fileBuf,bufBasic,10);
+        }
+        write(fileDes,fileBuf,strlen(fileBuf));
+}*/
 
 void runHandler(){
 	signal(SIGSEGV,handlerSegV);
@@ -61,7 +76,7 @@ void mtest1(){
 		exit(EXIT_FAILURE);
 	}
 	errno=0;
-	addr=mmap(NULL,length,PROT_READ,MAP_PRIVATE,fd1,offset);
+	addr=mmap(NULL,length*2,PROT_READ,MAP_PRIVATE,fd1,offset);
 	if(errno!=0){
 		fprintf(stderr,"Map Region Error: %s\n",strerror(errno));
 		exit(EXIT_FAILURE);
@@ -107,8 +122,99 @@ void mtest2(int a){
 		exit(1);
 	}
 }
-		
+
+void mtest4(int b){
+	int fd4=0;
+	char buf3[10]={'0','1','2','3','4','5','6','7','8','9'};
+	char buf4[4102];
+	struct stat testFile;
+	int originalSize=0;
+	int changedSize=0;
+	if(!(fd4=open("./mtext.txt",O_RDWR|O_CREAT|O_TRUNC,0666))){
+		fprintf(stderr,"Open/Create mtext.txt Error: %s\n",strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	int i=0;
+	for(i;i<410;i++){ 
+		strncat(buf4,buf3,10);
+	}
+	write(fd4,buf4,strlen(buf4));
+	//fileMaker(fd4,410,buf4);
+	if(fstat(fd4,&testFile)<0){
+		fprintf(stderr,"Uable to stat mtext.txt, Error: %s\n",strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	originalSize=(int)testFile.st_size;	
+	errno=0;
+	addr=mmap(NULL,4100,PROT_READ|PROT_WRITE,MAP_SHARED,fd4,offset);
+	if(errno!=0){
+		fprintf(stderr,"Map Region Error: %s\n",strerror(errno));
+		exit(EXIT_FAILURE);	
+	}
+	strcat(buf4,"*");
+	memcpy(addr,buf4,strlen(buf4));
+	if(b==4){
+		if(fstat(fd4,&testFile)<0){
+			fprintf(stderr,"Uable to stat mtext.txt, Error: %s\n",strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		changedSize=(int)testFile.st_size;
+		if(changedSize=originalSize){
+			exit(1);
+		}
+		else{
+			exit(0);
+		}
+	}
+	if(b==5){
+		char bufComp[2]={'\0','\0'};
+		int changedSizePro5=0;
+		offset=16;
+		lseek(fd4,offset,SEEK_END);
+		write(fd4,"E",sizeof(char));
+		offset=4100;
+		lseek(fd4,offset,SEEK_SET);
+		read(fd4,bufComp,sizeof(char));
+		if(strncmp(bufComp,"*",sizeof(char))==0){
+			exit(0);
+		}
+		else{
+			exit(1);
+		}
+	}
+}
+
+void mtest6(){
+	char buf6[5100];
+	char readBuf[2]={'\0','\0'};
+	int fd6=0;
+	if(!(fd6=open("./mtext.txt",O_RDWR|O_CREAT|O_TRUNC,0666))){
+		fprintf(stderr,"Open/Create mtext.txt Error: %s\n",strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	strcat(buf6,"This is a small file.");
+	write(fd6,buf6,strlen(buf6));
+	errno=0;
+	addr=mmap(NULL,8192,PROT_READ,MAP_SHARED,fd6,offset);
+	if(errno!=0){
+		fprintf(stderr,"Map Region Error: %s\n",strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	runHandler();
+	memcpy(buf6,addr,64*sizeof(char));
+	fprintf(stderr,"A Byte that beyond EOF is %x\n",buf6[50]);
+	memcpy(buf6,addr,5000*sizeof(char));
+	fprintf(stderr,"A Byte that beyond EOF is %x\n",buf6[4098]);
+}
+
 void main(int argc, char *argv[]){
+	char InitBuf[16];
+	strcpy(InitBuf,"For Memory Testing\0");
+	if(!(fd=open("./mtext.txt",O_RDWR|O_CREAT|O_TRUNC,0666))){
+		fprintf(stderr,"Open/Create mtext.txt Error: %s\n",strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	write(fd,InitBuf,strlen(InitBuf));
 	switch(atoi(argv[1])){
 		case 1:
 			mtest1();
@@ -118,6 +224,15 @@ void main(int argc, char *argv[]){
 			break;
 		case 3:
 			mtest2(3);
+			break;
+		case 4:
+			mtest4(4);
+			break;
+		case 5:
+			mtest4(5);
+			break;
+		case 6:
+			mtest6();
 			break;
 		default:
 			exit(1);
